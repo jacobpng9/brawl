3617981b113eeaba577d17840ad092b2c6276907
Jacob Ng <110934976+jacobpng9@users.noreply.github.com>
2024-07-24 01:30:41 -0500

    Delete middleware

--- next.config.js
+++ next.config.js
@@ -1,24 +1,28 @@
 // next.config.js
-module.exports = {
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        // matching all API routes
+        source: "/api/:path*",
         headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // You can specify specific origins instead of '*'
-          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            value: "GET,DELETE,PATCH,POST,PUT",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, content-type, Authorization",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
     ];
   },
 };
+
+module.exports = nextConfig;
